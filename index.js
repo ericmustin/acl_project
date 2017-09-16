52ff2e505a590ae780285a35b8f06131fa467cd8
emustin <ericmustin@github.com>
2017-09-16 00:11:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var pcap = require('pcap');
+var pcapSession = pcap.createSession('wlan0', 'wlan type mgt subtype probe-req');
+
+pcapSession.on('packet', function(rawPacket){
+  var packet = pcap.decode.packet(rawPacket);
+  console.log('from: ' + packet.link.ieee802_11Frame.shost);
+  console.log('to: ' + packet.link.ieee802_11Frame.dhost);
+  console.log('signal strength: ' + packet.link.ieee802_11Frame.strength);
+});
+
+http://www.hexacta.com/2015/04/09/how-to-listen-to-wifi-with-node-js/
